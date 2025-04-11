1fa3a164c651aca85457a6e976316c4e2574f988
Reyansh Singh <reyanshcomtech@gmail.com>
2025-04-11 11:48:09 +0530

    Add files via upload

---
+++ GitSystem/src/main/java/com/GitSystem/GitSystemApplication.java
@@ -0,0 +1,13 @@
+package com.GitSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSystemApplication.class, args);
+	}
+
+}
